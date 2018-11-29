5da424a63cd920e69246d90771e7270155702083
Robert <robertyeakeltroidl@gmail.com>
2018-11-28 18:29:50 -0800

    added weather display functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
+// Init UI
+const ui = new UI();
 
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -9,7 +11,7 @@ document.addEventListener('DOMContentLoaded', getWeather);
 function getWeather(){
   weather.getWeather()
     .then(results => {
-      console.log(results);
+      ui.paint(results);
     })
     .catch(err => console.log(err));
 }
